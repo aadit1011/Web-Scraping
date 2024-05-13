44e5f37a147bc4908d9b88d5912703e6465d8fa2
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-14 00:22:01 +0545

    Scrapping Prices and Courses
    Scrapping Prices and Courses

---
+++ Scrapping Prices and Courses .py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup;
+
+with open('D:\\Python\\Beautiful_Soup\\index.html','r') as file:
+          content=file.read();
+          soup=BeautifulSoup(content,'lxml');
+          content_course=soup.find_all('span');
+          print("The course contents in this site are=");
+          for items in content_course:
+               print(items.text);
+          course_price=soup.find_all('button');
+          print('And their respective prices are=')
+          for items in course_price:
+               print(items.text);
+          
